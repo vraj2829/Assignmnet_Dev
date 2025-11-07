386bc7caf8dd13e36c70be86bf78d7603676d3f5
vraj2829 <vrajpatel2829@gmail.com>
2025-11-07 23:50:54 +0530

    Add files via upload

--- shop/models.py
+++ shop/models.py
@@ -1,10 +1,10 @@
-# shop/models.py
 from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.FloatField()
     description = models.TextField()
+    image = models.URLField(blank=True, null=True)  # <-- Add this line
 
     def __str__(self):
         return self.name
